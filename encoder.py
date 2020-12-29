19e6eb8fe83dbe3981290dbc3f9b8cad170635f8
YDD <zhengyu.yu2@durham.ac.uk>
2020-12-29 17:36:56 +0000

    add the LZ77 decode function

--- encoder.py
+++ encoder.py
@@ -8,9 +8,14 @@ def file_compress(file):
     print("Starting encode...")
     f = open(file, "rb")
 
+    i = 0
     count = os.path.getsize(file)
     print(count)
-    prepare = bytes.decode(f.read()) #usding the read byte to make shure the encoding and decoding will be totally the same
+    buff = [b''] * int(count)
+    prepare = ''
+    prepare = bytes.decode(f.read())
+        #print(line)
+    #print(prepare)
 
     #compressing the LZ77
     if count != len(prepare):
@@ -47,6 +52,11 @@ def file_compress(file):
     else:
         l_bit_width = 1
 
+    print("lllppp:", l_bit_width, p_bit_width)
+    LZ77.write(int.to_bytes(l_bit_width, 1, byteorder='big'))
+    LZ77.write(int.to_bytes(p_bit_width, 1, byteorder='big'))
+
+    #print(word)
     for num in range(len(pointer)):
         LZ77.write(word[num].encode(encoding="utf-8"))
         LZ77.write(int.to_bytes(length[num], l_bit_width, byteorder='big'))
@@ -75,24 +85,14 @@ def file_compress(file):
         LZ78.write(word[num].encode(encoding="utf-8"))
         LZ78.write(int.to_bytes(pointer[num], bit_width, byteorder='big'))
 
-
+    #print(last)
+    #pickle.dump(w,j)
     LZ78.close()
     LZ77.close()
     print("LZ77 compressed size:", os.path.getsize("md_77.lz"))
     print("LZ78 compressed size:", os.path.getsize("md_78.lz"))
     f.close()
     return os.path.getsize(file)
-
-def file_uncompress(file):
-    print("Started decoding:")
-
-    #LZ78
-    unpick = LZ78_unpick(file)
-    unpress_LZ78 = uncompress_LZ78(unpick)
-    final_LZ78 = open("decode_78.tex", 'w')
-    final_LZ78.write(unpress_LZ78)
-    final_LZ78.close()
-    return os.path.getsize("decode_78.tex")
 
 
 def LZ_77(line, count):
@@ -171,12 +171,59 @@ def LZ_78(line):
                     yield (tree_dict.get(line[i:j + 1]), '')
                     i = j + 1
 
-def LZ78_unpick(file):
+
+def file_uncompress(file78, file77):
+    print("Started decoding:")
+
+    #LZ77
+    decode77 = LZ77_file_decode(file77)
+    unpress_LZ77 = uncompress_LZ77(decode77)
+    final_LZ77 = open("decode_77.tex", 'wb')
+    final_LZ77.write(unpress_LZ77.encode(encoding="utf-8"))
+    final_LZ77.close()
+
+    #LZ78
+    decode78 = LZ78_file_decode(file78)
+    unpress_LZ78 = uncompress_LZ78(decode78)
+    final_LZ78 = open("decode_78.tex", 'wb')
+    final_LZ78.write(unpress_LZ78.encode(encoding="utf-8"))
+    final_LZ78.close()
+
+    return os.path.getsize("decode_78.tex"), os.path.getsize("decode_77.tex")
+
+
+def LZ77_file_decode(file):
     f = open(file, "rb")
     i = 0
+
+    count = os.path.getsize(file)
+    l_b_width = int.from_bytes(f.read(1), byteorder='big')
+    p_b_width = int.from_bytes(f.read(1), byteorder='big')
+    message = []
+
+    while i < count:
+        word = f.read(1).decode(encoding="utf-8")
+        length = int.from_bytes(f.read(l_b_width), byteorder='big')
+        pointer = int.from_bytes(f.read(p_b_width), byteorder='big')
+        message.append((pointer, length, word))
+        i = i + p_b_width + l_b_width + 1
+    return message
+
+
+def uncompress_LZ77(message):
+    de_msg = ''
+    for s in message:
+        if s[0] != 0:
+            de_msg += de_msg[(len(de_msg) - s[0]): (len(de_msg) - s[0] + s[1])]
+        de_msg += s[2]
+    return de_msg
+
+def LZ78_file_decode(file):
+    f = open(file, "rb")
+    i = 0
+    count = os.path.getsize(file)
     b_width = int.from_bytes(f.read(1), byteorder='big')
     i += 1
-    count = os.path.getsize(file)
     while i < count:
         word = f.read(1).decode(encoding="utf-8")
         pointer = int.from_bytes(f.read(b_width), byteorder='big')
@@ -201,8 +248,8 @@ def uncompress_LZ78(packed):
 def main():
     size = file_compress("test.tex")
     print ("encode size: ", size)
-    size_78 = file_uncompress("md_78.lz")
-    print("LZ78 decode size:", size_78, '\n', "original size:", os.path.getsize("test.tex"))
+    size_78, size_77 = file_uncompress("md_78.lz", "md_77.lz")
+    print("LZ78 decode size:", size_78, '\n', "LZ77 decode size:", size_77, '\n',  "original size:", os.path.getsize("test.tex"))
 
 if __name__ == "__main__":
     main()
