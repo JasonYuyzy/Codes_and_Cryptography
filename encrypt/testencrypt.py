f4ded7ecb53c1e95fe508bcf302de067dbccced5
Jason Yu <zhengyu.yu2@durham.ac.uk>
2021-01-02 03:42:55 +0000

    finish the encryption file (words searching)

--- encrypt/testencrypt.py
+++ encrypt/testencrypt.py
@@ -1,18 +1,45 @@
 import os
+import time
 import subprocess
-from subprocess import Popen, PIPE
 
-location = "./encrypt.exe"
+#goal: find the str: 903408ec4d951acfaeb47ca88390c475
+check_str = '903408ec4d951acfaeb47ca88390c475'
 
-os.system(location + ' "0123456789abcdef"')
+def load_words():
+    with open('words_alpha.txt') as word_file1:
+        valid_words = word_file1.read().split()
+    return valid_words
 
-main = r'./encrypt.exe "0123456789abcdef"'  # exe文件的绝对路径
-a = subprocess.getstatusoutput(main)
-print(a)
+def check_if_right(hex_code):
+    check = r'encrypt.exe "' + hex_code + '"'  # exe文件的绝对路径
+    a = subprocess.getstatusoutput(check)
+    if a[1] == check_str:
+        return True
+    else:
+        False
 
-'''
-p= Popen([location],stdin=PIPE,stdout=PIPE,stderr=PIPE, encoding="UTF8")
-command='START'
-p.stdin.write(command)
-response=p.stdout.readine()
-'''
+def main():
+    english_words = load_words()
+    word_list1 = english_words
+    word_list2 = english_words
+    word_list3 = english_words
+
+    list_length = len(english_words)
+    starts = time.time()
+    for i in range(list_length):
+        for j in range(list_length):
+            for k in range(list_length):
+                combine = word_list1[i] + '.' + word_list2[j] + '.' + word_list3[k]
+                #combine = 'tile' + '.' + 'bills' + '.' + 'print'
+                location = check_if_right(combine.encode().hex())
+                if location:
+                    print("the final location is:", combine)
+                    ends = time.time()
+                    time_cost = ends - starts
+                    print("searching time:", time_cost)
+                    print("searching steps:", i + j + k)
+                    exit()
+
+
+if __name__ == '__main__':
+    main()
