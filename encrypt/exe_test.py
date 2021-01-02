5db39f8bf9009490a23a82d2f85f17ca50758b44
YDD <zhengyu.yu2@durham.ac.uk>
2021-01-02 20:57:32 +0000

    change the test way

--- encrypt/exe_test.py
+++ encrypt/exe_test.py
@@ -1,6 +1,8 @@
 import os
 import time
 import subprocess
+import progressbar
+p = progressbar.ProgressBar()
 
 check_str = '903408ec4d951acfaeb47ca88390c475'
 
@@ -12,14 +14,14 @@ def load_words():
 def check_if_right(hex_code, encode_len):
     check = r'encrypt.exe "' + hex_code + '"'  # exe文件的绝对路径
     a = subprocess.getstatusoutput(check)
-    if len(a[1]) != encode_len:
-        return True
+    if len(a[1]) == encode_len:
+        return True, a[1]
     else:
         False
 
 def main():
+    f = open("bad_words_a.txt", "a+")
     word_list = list()
-    code_list = list()
     english_words = load_words()
     for w in english_words:
         if w[0] == 'a':
@@ -28,35 +30,22 @@ def main():
     list_length = len(word_list)
     print("words length:", list_length)
     print("total words:", list_length ** 3)
-    compare_list = list()
+    p.start(1000)
+    count = 0
     for i in range(list_length):
         for j in range(list_length):
             for k in range(list_length):
                 combine = word_list1[i] + '.' + word_list1[j] + '.' + word_list1[k]
                 hex_code = combine.encode().hex()
-                code_list.append(hex_code)
-        print("{:.2%} combined...".format(list_length ** 2 / (list_length ** 3)))
+                encoded, code = check_if_right(hex_code, len(hex_code))
+                if encoded:
+                    f.write(combine + ":" + code.decode() + '\n')
+                    count += 1
+                    if count == 1000:
+                        f.close()
+                        exit()
 
-    count = 0
-    f = open("bad_words_a.txt", "a+")
-    for code in code_list:
-        useless = check_if_right(code, len(hex_code))
-        if useless:
-            if word_list1[i] not in compare_list:
-                compare_list.append(word_list1[i])
-                f.write(word_list1[i] + '\n')
-            if word_list1[j] not in compare_list:
-                compare_list.append(word_list1[j])
-                f.write(word_list1[i] + '\n')
-            if word_list1[k] not in compare_list:
-                compare_list.append(word_list1[k])
-                f.write(word_list1[k] + '\n')
-        if len(compare_list) == len(code_list):
-            print("finished early")
-            exit()
-        count += 1
-        if count % 15 == 0:
-            print("About {:.2%} search done so far...".format(count / len(code_list)))
+                p.update(count)
     f.close()
     exit()
 
