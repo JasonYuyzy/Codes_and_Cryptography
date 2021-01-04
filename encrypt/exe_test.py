d8648bd5d17492f1e568eab0df55daff4a9b855c
Jason Yu <zhengyu.yu2@durham.ac.uk>
2021-01-04 00:09:53 +0000

    edit the test code

--- encrypt/exe_test.py
+++ encrypt/exe_test.py
@@ -4,7 +4,7 @@ import random
 import subprocess
 import progressbar
 from rich.progress import track
-p1 = progressbar.ProgressBar()
+p = progressbar.ProgressBar()
 
 check_str = '903408ec4d951acfaeb47ca88390c475'
 
@@ -24,7 +24,7 @@ def main():
     word_list2 = list()
     word_list3 = list()
     total = 1200
-    head = 'a b c'
+    head = 'til bill prin'
     head_list = head.split(' ')
     w1, w2, w3 = head_list[0], head_list[1], head_list[2]
     english_words = load_words()
@@ -34,11 +34,11 @@ def main():
         if len(w) > 3:
             if len(w) < 7:
                 if w != 'fuck' and w != 'shit':
-                    if w[0] == w1:
+                    if w[0:3] == w1:
                         word_list1.append(w)
-                    if w[0] == w2:
+                    if w[0:4] == w2:
                         word_list2.append(w)
-                    if w[0] == w3:
+                    if w[0:4] == w3:
                         word_list3.append(w)
 
     list_length1 = len(word_list1)
@@ -48,23 +48,25 @@ def main():
     print("words start with {0} : {1}".format(w2, list_length2))
     print("words start with {0} : {1}".format(w3, list_length3))
     write_in = 0
+    '''
     for i in range(list_length1):
         for j in range(list_length2):
             for k in track(range(total)):
                 first = random.randint(0, list_length1 - 1)
                 second = random.randint(0, list_length2 - 1)
+                third = random.randint(0, list_length3 - 1)
                 if k < total/6:
-                    combine = word_list1[first] + '.' + word_list2[second] + '.' + word_list3[random.randint(0, list_length3-1)]
+                    combine = word_list1[first] + '.' + word_list2[second] + '.' + word_list3[third]
                 elif k >= total/6 and k < (total*2)/6:
-                    combine = word_list3[first] + '.' + word_list1[second] + '.' + word_list2[random.randint(0, list_length3-1)]
+                    combine = word_list3[third] + '.' + word_list1[first] + '.' + word_list2[second]
                 elif k >= (total*2)/6 and k < (total*3)/6:
-                    combine = word_list3[first] + '.' + word_list2[second] + '.' + word_list1[random.randint(0, list_length3-1)]
+                    combine = word_list3[third] + '.' + word_list2[second] + '.' + word_list1[first]
                 elif k >= (total*3)/6 and k < (total*4)/6:
-                    combine = word_list2[first] + '.' + word_list1[second] + '.' + word_list3[random.randint(0, list_length3-1)]
+                    combine = word_list2[second] + '.' + word_list1[first] + '.' + word_list3[third]
                 elif k >= (total*4)/6 and k < (total*5)/6:
-                    combine = word_list1[first] + '.' + word_list3[second] + '.' + word_list2[random.randint(0, list_length3-1)]
+                    combine = word_list1[first] + '.' + word_list3[third] + '.' + word_list2[second]
                 else:
-                    combine = word_list2[first] + '.' + word_list3[second] + '.' + word_list1[random.randint(0, list_length3-1)]
+                    combine = word_list2[second] + '.' + word_list3[third] + '.' + word_list1[first]
                 if len(combine) == 16:
                     hex_code = combine.encode().hex()
                     encoded, code = check_if_right(hex_code, len(hex_code))
@@ -72,9 +74,26 @@ def main():
                         out = combine + ":"
                         f.write(out.ljust(20, ' ') + code + '\n')
                         write_in += 1
-
-    f.close()
+                if k == total - 1:
+                    print("There are totally {0} recorded".format(write_in))
+                    exit()'''
+    p.start(list_length3 * list_length1 * list_length2)
+    count = 0
+    for i in range(list_length1):
+        for j in range(list_length2):
+            for k in range(list_length3):
+                combine = word_list1[i] + '.' + word_list2[j] + '.' + word_list3[k]
+                if len(combine) == 16:
+                    hex_code = combine.encode().hex()
+                    encoded, code = check_if_right(hex_code, len(hex_code))
+                    if encoded == 1:
+                        out = combine + ":"
+                        f.write(out.ljust(20, ' ') + code + '\n')
+                        write_in += 1
+                count += 1
+                p.update(count)
     print("There are totally {0} recorded".format(write_in))
+    f.close()
     exit()
 
 
